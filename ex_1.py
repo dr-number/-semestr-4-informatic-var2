8ef700dbdc4457659dd3369a4088a02c2c547cfd
dr.number <dr.number@yandex.ru>
2023-05-01 16:35:06 +0700

    adding ex 1

--- ex_1.py
+++ ex_1.py
@@ -2,15 +2,11 @@ from helpers import get_text_color, input_number, COLOR_WARNING, COLOR_GREEN, CO
 
 def init():
     print(
-        f'Вычислить уровень расчетной рентабельности по формуле: {get_text_color("Kpp = P/T", COLOR_GREEN)} где\n'
-        f'{get_text_color("P = Pб - Pfe - Pf - Pk", COLOR_GREEN)};\n'
-        f'{get_text_color("T = F1 + En + Vn", COLOR_GREEN)}\n\n'
-        f'{get_text_color("Pб", COLOR_GREEN)} - балансовая прибыль;\n'
-        f'{get_text_color("Pfe", COLOR_GREEN)} - плата за фонды;\n'
-        f'{get_text_color("Pf", COLOR_GREEN)} - финансируемые платежи;\n'
-        f'{get_text_color("Pk", COLOR_GREEN)} - процент за кредит;\n'
-        f'{get_text_color("F1", COLOR_GREEN)} - средняя стоимость основных фондов;\n'
-        f'{get_text_color("En", COLOR_GREEN)} - нормируемые оборотные средства;\n'
-        f'{get_text_color("Vn", COLOR_GREEN)} - сверхплановые запасы неустановленного оборудования.\n\n'
+        f'Вычислить на заданном интервале значения функции:\n'
+        f'        | √(x + 2),   если 0 < x <= 1\n'
+        f'y(x) = <  ln(x - 1),  если x > 1\n'
+        f'        | 0,          если x <= 0\n\n'
+        f'где - 2 <= x <= 2.2;  dx = 0.2\n\n'
+        f'На печать вывести значения x и y в виде таблицы с заголовком\n'
     )
     
