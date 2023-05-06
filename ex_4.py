4b856058ac6de79273357c068d6e07b9f69d0514
dr.number <dr.number@yandex.ru>
2023-05-06 23:20:44 +0700

    add ex 4

--- ex_4.py
+++ ex_4.py
@@ -1,4 +1,7 @@
 from helpers import get_text_color, input_number, COLOR_WARNING, COLOR_GREEN, COLOR_FAIL
 
 def init():
-    pass
+    print(
+       f'Дан массив: {get_text_color("A=(0.1, -2.4, 12.56, -0.5, 0.4)", COLOR_GREEN)}\n' 
+       f'Выдать на печать массив B , все элементы которого определяются по правилу\n'
+    )
