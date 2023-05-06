195cdde94e1e847ef93d5b76ccb73012bfc007d7
dr.number <dr.number@yandex.ru>
2023-05-06 15:59:54 +0700

    adding functions for matrix

--- helpers.py
+++ helpers.py
@@ -1,3 +1,5 @@
+import random
+
 COLOR_GREEN = '\033[92m'
 COLOR_WARNING = '\033[93m'
 COLOR_OKCYAN = '\033[96m'
@@ -20,11 +22,14 @@ def is_number(str)-> bool:
     except ValueError:
         return False
         
-def input_number(text: str, default_value: float, min: float = None, max: float = None)-> float:
+def input_number(text: str, default_value: float = None, min: float = None, max: float = None)-> float:
     while True:
         number = input(f'{get_text_color(text, COLOR_WARNING)} ')
         if number == '':
-            return default_value
+            if default_value is not None:
+                return default_value
+            else:
+                print(get_text_color(f"Введите число!", COLOR_FAIL))
         elif is_number(number):
             if min is not None and float(number) < min:
                 print(get_text_color(f"Минимально допустимое число - {min}!", COLOR_FAIL))
@@ -40,18 +45,43 @@ def input_number(text: str, default_value: float, min: float = None, max: float 
 def is_question(text: str)-> bool:
     return input(f'{get_text_color(text, COLOR_WARNING)} [Y/n]: ').lower() != 'n' 
 
-def create_matrix(rows: int, cols: int):
+def create_matrix(rows: int, cols: int, min_value: int, max_value: int):
+    result = []
     is_hand = is_question('Ввести матрицу вручную?')
-    if is_hand:
-        rows = int(input_number(
-            text=f'Введите количество строк: (по умолчанию {rows})',
-            default_value=rows,
-            min=_MATRIX_MIN_ROWS,
-            max=_MATRIX_MAX_ROWS
-        ))
-        cols = int(input_number(
-            text=f'Введите количество строк: (по умолчанию {cols})',
-            default_value=cols,
-            min=_MATRIX_MIN_COLS,
-            max=_MATRIX_MAX_COLS
-        ))
+    if not is_hand:
+        for i in range(1, rows):
+            row = []
+            for j in range(1, cols):
+                row.append(random.uniform(min_value, max_value))
+            result.append(row)
+        return result
+
+    rows = int(input_number(
+        text=f'Введите количество строк: (по умолчанию {rows})',
+        default_value=rows,
+        min=_MATRIX_MIN_ROWS,
+        max=_MATRIX_MAX_ROWS
+    ))
+    cols = int(input_number(
+        text=f'Введите количество строк: (по умолчанию {cols})',
+        default_value=cols,
+        min=_MATRIX_MIN_COLS,
+        max=_MATRIX_MAX_COLS
+    ))
+
+    for i in range(1, rows):
+        row = []
+        for j in range(1, cols):
+            row.append(
+                int(
+                    input_number(
+                        text=f'Введите значение (ряд {i} из {rows}, столбец {j} из {cols}): ', 
+                        min=min_value,
+                        max=max_value
+                    )
+                )
+            )
+        result.append(row)
+    return result
+
+    
