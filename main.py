6d1a97f7727fad4ee59d9548ffbe0048cdba88f0
dr.number <dr.number@yandex.ru>
2023-05-06 19:41:35 +0700

    add ex 8

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def main():
             # f"{get_text_color(f'{_EX_5}) ', COLOR_WARNING)}Вычислить значения функции\n"
             f"{get_text_color(f'{_EX_6}) ', COLOR_WARNING)}Дана матрицa B 3*3. Заменить отрицательные элементы матрицы нулями\n"
             f"{get_text_color(f'{_EX_7}) ', COLOR_WARNING)}Дана матрица A. Вычислить элементы массива Y по правилу.\n"
-            f"{get_text_color(f'{_EX_8}) ', COLOR_WARNING)}\n"
+            f"{get_text_color(f'{_EX_8}) ', COLOR_WARNING)}Сформировать двумерный массив M таким образом, чтобы \"углы\" и \"центр\" были заполнены нулями, а остальные элементы должны быть равны 1\n"
         )
         select = input('Для выхода введите \'0\'\n')
 
