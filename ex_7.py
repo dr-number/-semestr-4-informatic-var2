7ddd1f7a4308c13b4ca6c05a96e6dfc1f388e97c
dr.number <dr.number@yandex.ru>
2023-05-06 19:03:22 +0700

    adding ex 7

--- ex_7.py
+++ ex_7.py
@@ -1,9 +1,18 @@
-from helpers import get_text_color, input_number, COLOR_WARNING, COLOR_GREEN, COLOR_FAIL
+from helpers import get_text_color, create_matrix, print_matrix, COLOR_GREEN, COLOR_WARNING
 
 _DEFAULT_ROWS = 3
 _DEFAULT_COLS = 5
 _MIN_VALUE = -20
 _MAX_VALUE = 20
+
+def _sum_positive_col(matrix, col)-> int:
+    rows = len(matrix)
+    _sum = 0
+    for i in range(0, rows):
+        if matrix[i][col] > 0:
+            _sum += matrix[i][col]
+
+    return _sum
 
 def init():
     print(
@@ -12,3 +21,27 @@ def init():
         f'{get_text_color("y[j] = Sin(x[j])", COLOR_GREEN)}, где\n'
         'x[j] - сумма положительных элементов j-го столбца матрицы A.\n'
     )
+    matrix = create_matrix(
+        rows=_DEFAULT_ROWS,
+        cols=_DEFAULT_COLS,
+        min_value=_MIN_VALUE,
+        max_value=_MAX_VALUE
+    )
+    if not matrix:
+        return
+
+    print(get_text_color('\nМатрица A', COLOR_GREEN))
+    print_matrix(matrix=matrix)
+
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    result_1 = []
+    for _ in range(0, rows):
+        row = []
+        for j in range(0, cols):
+            row.append(_sum_positive_col(matrix=matrix, col=j))
+        result_1.append(row)
+
+    print(get_text_color('\nПромежуточная матрица (без функции Sin)', COLOR_WARNING))
+    print_matrix(matrix=result_1)
