133010b5e792ca42830282cf46f38ed15d71cbcf
dr.number <dr.number@yandex.ru>
2023-05-06 19:40:15 +0700

    add ex 8

--- ex_8.py
+++ ex_8.py
@@ -1,4 +1,15 @@
 from helpers import get_text_color, input_number, COLOR_WARNING, COLOR_GREEN, COLOR_FAIL
 
 def init():
-    pass
+    example = (
+        "    | 0 0 1 0 0 |\n"
+        "    | 0 1 1 1 0 |\n"
+        "M = | 1 1 0 1 1 |\n"
+        "    | 0 1 1 1 0 |\n"
+        "    | 0 0 1 0 0 |\n"
+    )
+    print(
+        'Сформировать двумерный массив M(n,n) (где n - нечетное число) таким образом, чтобы "углы" ' 
+        'и "центр" были заполнены нулями, а остальные элементы должны быть равны 1, т.е\n'
+        f'{get_text_color(example, COLOR_GREEN)}\n'
+    )
