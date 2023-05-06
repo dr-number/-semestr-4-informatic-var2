335b66da0b75e7561e194c85520fdfca92f9e444
dr.number <dr.number@yandex.ru>
2023-05-06 15:28:14 +0700

    adding ex 5

--- ex_5.py
+++ ex_5.py
@@ -1,4 +1,8 @@
 from helpers import get_text_color, input_number, COLOR_WARNING, COLOR_GREEN, COLOR_FAIL
 
 def init():
-    pass
+    print(
+        f'Вычислить значения функции: {get_text_color("f= G(x) + R(y)", COLOR_GREEN)}, где\n'
+        f'G(x) = ( sin(x) + 2 ) / ( sin(x + 2/3 * ПИ) ),    -ПИ/2 <= x <= ПИ/2,     dx = ПИ/8\n'
+        f'R(y) = (2 * sin(y)) / (cos^3(y)),     1 <= y <= 2; dy = 0.25'
+    )
