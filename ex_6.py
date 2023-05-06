43ea1f5bd3af260fc718378bc66d9e674ce5ecfa
dr.number <dr.number@yandex.ru>
2023-05-06 18:28:19 +0700

    correct matrix

--- ex_6.py
+++ ex_6.py
@@ -1,4 +1,4 @@
-from helpers import create_matrix, print_matrix, get_text_color, COLOR_GREEN
+from helpers import create_matrix, print_matrix, get_text_color, COLOR_GREEN, COLOR_FAIL
 
 _DEFAULT_ROWS = 3
 _DEFAULT_COLS = 3
@@ -16,5 +16,26 @@ def init():
         min_value=_MIN_VALUE,
         max_value=_MAX_VALUE
     )
+    if not matrix:
+        return
+
     print(get_text_color('\nИсходная матрица', COLOR_GREEN))
     print_matrix(matrix=matrix, select_negative=True)
+
+    rows = len(matrix)
+    cols = len(matrix[0])
+    count_replace_elements = 0
+
+    for i in range(0, rows):
+        for j in range(0, cols):
+            if matrix[i][j] < 0:
+                matrix[i][j] = 0
+                count_replace_elements += 1
+
+    print(get_text_color('\nКонечная матрица', COLOR_GREEN))
+    print_matrix(matrix=matrix, select_zero=True)
+
+    if count_replace_elements == 0:
+        print(get_text_color(f"\nВ исходной матрице нет отрицательных элементов!", COLOR_FAIL))
+    else:
+        print(f"\nЗаменено {get_text_color(str(len(count_replace_elements)), COLOR_GREEN)} отрицательных элементов")
